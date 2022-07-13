f78020eb1c99a189e66e6c4fa825786ea5683138
LAPTOP-A5M7S5QL\joyst <joystonf@gmail.com>
2022-07-13 08:01:06 +0530

    -updated add function -Added PrintList function

--- main.go
+++ main.go
@@ -1,33 +1,40 @@
 package main
 
+import "fmt"
+
 type List[T any] struct {
 	next *List[T]
 	val  T
 }
 
 func (l *List[T]) Add(aVal T) {
+	for l.next != nil {
+		l = l.next
+	}
+
 	temp := List[T]{}
 
 	if l.next == nil {
 		l.val = aVal
 		l.next = &temp
-	} else {
-		l.next.val = aVal
-		l.next.next = &temp
 	}
-
 }
 
 //Function to print the list
 func (l *List[T]) PrintList() {
-
+	for l.next != nil {
+		fmt.Println(l.val)
+		l = l.next
+	}
 }
 
 func main() {
 	l := List[string]{}
 	l.Add("foo")
 	l.Add("bar")
-	l.Add("savinf")
+	l.Add("saving")
 	l.Add("private")
 	l.Add("ryan")
+
+	l.PrintList()
 }
